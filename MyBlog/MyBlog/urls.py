3ca70c4cc1cac5aa1a9a3da99546174d7e875e7a
xc <727790922@qq.com>
2018-04-02 17:07:14 +0800

    add ckeditor-image

--- MyBlog/MyBlog/urls.py
+++ MyBlog/MyBlog/urls.py
@@ -19,4 +19,5 @@ from django.conf.urls import url,include
 urlpatterns = [
     url('admin/', admin.site.urls),
     url(r'^xc_Blog/',include('blog.urls')),
+    url(r'ckeditor/', include('ckeditor_uploader.urls'))
 ]
--- MyBlog/blog/models.py
+++ MyBlog/blog/models.py
@@ -1,9 +1,10 @@
 from ckeditor.fields import RichTextField
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 class Blog(models.Model):
+    # user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=False)
     title = models.CharField(max_length=50, verbose_name="标题")
     content = RichTextField(blank=True, null=True, verbose_name="内容")
 
